266677dcf9254402bdf273232347080ccca5c8e1
CrazyMax <1951866+crazy-max@users.noreply.github.com>
2026-02-16 18:15:39 +0100

    migrate to vitest/eslint-plugin and move tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,19 +1,16 @@
 import {defineConfig} from 'vitest/config';
 
 export default defineConfig({
-  resolve: {
-    alias: [{find: /^(\.{1,2}\/.*)\.js$/, replacement: '$1'}]
-  },
   test: {
     environment: 'node',
     setupFiles: ['dotenv/config'],
-    include: ['**/*.test.ts'],
+    include: ['tests/**/*.test.ts'],
     coverage: {
       provider: 'v8',
       reporter: ['text', 'clover', 'lcov'],
       reportsDirectory: 'coverage',
       include: ['src/**/*.ts'],
-      exclude: ['src/main.ts', 'dist/**', 'node_modules/**', '__tests__/**']
+      exclude: ['src/main.ts']
     }
   }
 });
