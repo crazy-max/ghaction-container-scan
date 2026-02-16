3d2febc9784500ff09e7f3e24e4d5f790e6e0543
CrazyMax <1951866+crazy-max@users.noreply.github.com>
2026-02-17 00:47:52 +0100

    migrate prettier to ESM config and update ignore patterns

---
+++ prettier.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('prettier').Config} */
+export default {
+  printWidth: 240,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'none',
+  bracketSpacing: false,
+  arrowParens: 'avoid'
+};
